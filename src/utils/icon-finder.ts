b67462176568a43293247b373a624c36d2b6460e
Juan Bernal <juandiegobernalb@gmail.com>
2017-10-18 20:56:07 -0500

    icon finder implemented

--- src/utils/icon-finder.ts
+++ src/utils/icon-finder.ts
@@ -1,7 +1,42 @@
-import { GitHubEntity, GitHubEntityType } from './github-entity';
+import { GitHubEntity, GitHubEntityType } from "./github-entity";
+import { extensions as files } from "./vscode-icon-manifest/supportedExtensions";
+import { extensions as folders } from "./vscode-icon-manifest/supportedFolders";
+import { languages } from "./vscode-icon-manifest/languages";
 
+const folderNames = folders.supported.reduce((init, current) => {
+  const obj = init;
+  if (current.extensions.length)
+    current.extensions.forEach(ext => {
+      obj[ext] = current.icon;
+    });
+  return obj;
+}, {});
+const fileNames = files.supported.reduce((init, current) => {
+  const obj = init;
+  if (!obj.hasOwnProperty("extensions")) obj["extensions"] = {};
+  if (!obj.hasOwnProperty("filenamesGlob")) obj["filenamesGlob"] = {};
+  if (!obj.hasOwnProperty("languages")) obj["languages"] = {};
+  if (current.hasOwnProperty("extensions")) {
+    current.extensions.forEach(ext => {
+      obj["extensions"][ext] = current.icon;
+    });
+  }
+  if (current.hasOwnProperty("filenamesGlob"))
+  if (current.hasOwnProperty("extensionsGlob")) {
+    
+    current.extensionsGlob.forEach(extG => {
+      current.filenamesGlob.forEach(fileG => {
+        obj["filenamesGlob"][fileG+'.'+extG] = current.icon;
+      });
+    });
+  }
+  if (current.hasOwnProperty("languages"))
+    current.languages.forEach(ext => {
+          obj["languages"][ext.defaultExtension] = current.icon;
+    });
+  return obj;
+}, {});
 export class IconFinder {
-
   /**
    * Returns the new icon name for an specified entity.
    * This method does not return the full URL of the resource, though.
@@ -12,9 +47,21 @@ export class IconFinder {
    * @memberof IconFinder
    */
   getIconName(entity: GitHubEntity): string {
-    // TODO
-    // Dummy code that returns the Jenkins logo if the entity is a file
-    // Replace this for the real icon finder algorithm
-    return entity.type === GitHubEntityType.File ? 'file_type_jenkins.svg' : undefined;
+
+    if (entity.type === GitHubEntityType.Folder) {
+      return folderNames[entity.fileName]
+        ? "folder_type_" + folderNames[entity.fileName] + ".svg"
+        : "default_folder.svg";
+    } else {
+      console.log(entity.fileName)
+      console.log(fileNames["languages"])
+      return fileNames["extensions"][entity.fileName]
+        ? "file_type_" + fileNames["extensions"][entity.fileName] + ".svg"
+        : (fileNames["filenamesGlob"][entity.fileName]
+          ? "file_type_" + fileNames["filenamesGlob"][entity.fileName] + ".svg"
+          : (fileNames["languages"][entity.fileName.split('.')[1]]
+          ? "file_type_" + fileNames["languages"][entity.fileName.split('.')[1]] + ".svg"
+          : "default_file.svg"));
+    }
   }
 }
